07a5cefe59b16e854b2482e29e21841de5bea700
vvenkat123 <105635316+vvenkat123@users.noreply.github.com>
2023-07-13 23:04:38 +0530

    Update streamlit_app.py
    added breakfast favorites

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,15 @@ streamlit.title('My parents new healthy diner')
 
 streamlit.header ('Breakfast Menu')
 
-streamlit.text('Omega 3 & Blusberry 03tneal')
+streamlit.text('Omega 3 & Blueberry 03tneal')
 streamlit.text('Kalo, Spinach & Rocket Swoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
+
+streamlit.header('Breakfast Favorites')
+
+
+streamlit.text('🥣 Omega 3 & Blueberry Oatneal')
+streamlit.text(" 🥗Kale, Spinach & Rocket Snoothie')
+streamlit.text('🐔Hard-Bolled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
